3a3b5c40fd3c25cf5f153908aceda41ba424efaa
maximkwski <makkie.makowski@gmail.com>
2022-08-11 20:25:20 -0400

    done some regex stuff working

--- myscripts.js
+++ myscripts.js
@@ -14,22 +14,28 @@ const phone = document.getElementById('phone')
 const lang = document.getElementById('lang')
 const errorElement = document.getElementById('error')
 
-
+pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
 
 form.addEventListener('submit', (e) => { 
-    let validEmail = /^\S+@\S+\.\S+$/
     let messages = []
     // if (nname.value === '' || nname.value == null) {
-    //     messages.push("Name is required")
-    // }
-    // if (nname.value.length < 3 && nname.value.length >= 1) {
-    //     messages.push('Name is too short')
-    // }
-
-    if (email.value.match(/^\S+@\S+\.\S+$/)) {
-        messages.push ('Please enter a valid email address')
+        //     messages.push("Name is required")
+        // }
+        // if (nname.value.length < 3 && nname.value.length >= 1) {
+            //     messages.push('Name is too short')
+            // }
+            
+    
+    if (/^\S+@\S+\.\S+$/.test(email.value) === false) {
+        messages.push('Please enter a valid email pleaaseeee')
     }
 
+
+    let validPhone = /[0-9]{3}-[0-9]{3}-[0-9]{4}/;
+    
+    if (/[0-9]{3}-[0-9]{3}-[0-9]{4}/.test(phone.value) === false) {
+        messages.push("Enter a phone number in a valid format")
+    }
     // if (lang.value === '') {
     //     messages.push ("Please select a language.")
     // }
@@ -37,6 +43,6 @@ form.addEventListener('submit', (e) => {
     if (messages.length > 0){
         e.preventDefault()
         errorElement.innerText = messages.join(', ')
+        // return false;
     }
-    
 }) 
